6900f5f60018dc30ac86d8582f23aba0c7fac4bc
valentinerutto <rutto28@gmail.com>
2015-10-03 02:39:50 +0300

    added tab1 on main

--- app/src/main/java/com/example/valentine/hardwell/MainActivity.java
+++ app/src/main/java/com/example/valentine/hardwell/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.valentine.hardwell;
 
+import android.app.Activity;
+import android.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,8 +18,17 @@ public class MainActivity extends AppCompatActivity {
         android.app.ActionBar actionBar= getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
+        String label1=getResources().getString(R.string.label1);
+        android.app.ActionBar.Tab tab= actionBar.newTab();
+        tab.setText(label1);
     }
-
+private class TabListener<T extends Fragment>implements ActionBar.TabListener{
+    private Fragment mFragment;
+    private final Activity mActivity;
+    private final String
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, Fragment);
+};
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
