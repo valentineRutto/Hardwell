9c94bad4760d546f16cab5aeaee80f48c68fdcdc
valentinerutto <rutto28@gmail.com>
2015-10-03 21:00:22 +0300

    populated tab1ntab2

--- app/src/main/java/com/example/valentine/hardwell/MainActivity.java
+++ app/src/main/java/com/example/valentine/hardwell/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.valentine.hardwell;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,20 +16,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        android.app.ActionBar actionBar= getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-        String label1=getResources().getString(R.string.label1);
-        android.app.ActionBar.Tab tab= actionBar.newTab();
-        tab.setText(label1);
     }
-private class TabListener<T extends Fragment>implements ActionBar.TabListener{
-    private Fragment mFragment;
-    private final Activity mActivity;
-    private final String
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, Fragment);
-};
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
