46d627ce9159e6bc5a05f0b1e0bb41f1dde76458
valentinerutto <rutto28@gmail.com>
2015-10-04 12:46:08 +0300

    inflate tablayouts and setupviews

--- app/src/main/java/com/example/valentine/hardwell/TabFragment.java
+++ app/src/main/java/com/example/valentine/hardwell/TabFragment.java
@@ -1,9 +1,30 @@
 package com.example.valentine.hardwell;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by valentine on 10/4/15.
  */
 public class TabFragment extends Fragment {
-}
+    public static TabLayout tabLayout;
+    public static ViewPager viewPager;
+    public static int int_items = 3 ;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View x = inflater.inflate(R.layout.tab_layout, null);
+        tabLayout = (TabLayout) x.findViewById(R.id.tabs);
+        viewPager = (ViewPager) x.findViewById(R.id.viewpager);
+
+
+    }
+    }
