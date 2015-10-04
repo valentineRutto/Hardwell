f9ab9d7e41ac7e9deb9e08f52555649245a3266c
valentinerutto <rutto28@gmail.com>
2015-10-04 13:51:49 +0300

    include youtubeplayeractivity dependencies

--- app/src/main/java/com/example/valentine/hardwell/PrimaryFragment.java
+++ app/src/main/java/com/example/valentine/hardwell/PrimaryFragment.java
@@ -15,5 +15,7 @@ public class PrimaryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.primary_layout,null);
+
+        
     }
 }
