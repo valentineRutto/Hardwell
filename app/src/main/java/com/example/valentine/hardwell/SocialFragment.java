e152f99931cc060657abc60164b3f4ada034e312
valentinerutto <rutto28@gmail.com>
2015-10-05 20:09:56 +0300

    set about cards bg colors

--- app/src/main/java/com/example/valentine/hardwell/SocialFragment.java
+++ app/src/main/java/com/example/valentine/hardwell/SocialFragment.java
@@ -1,9 +1,21 @@
 package com.example.valentine.hardwell;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by valentine on 10/4/15.
  */
 public class SocialFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.mixxes,null);
+
+
+    }
 }
--- app/src/main/java/com/example/valentine/hardwell/UpdatesFragment.java
+++ app/src/main/java/com/example/valentine/hardwell/UpdatesFragment.java
@@ -6,4 +6,5 @@ import android.support.v4.app.Fragment;
  * Created by valentine on 10/4/15.
  */
 public class UpdatesFragment extends Fragment {
+
 }
